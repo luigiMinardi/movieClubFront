002f03ffd58955a5424eb00819f033b26833f06b
Luigi Minardi <lugminsinlag@gmail.com>
2022-03-20 16:43:15 +0100

    feat: Validating custom onClick function at Button component

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -11,7 +11,27 @@ const Button = (props) => {
     }
 
     const sendMeOrCustomFunction = (func = sendMe, param = '/') => {
-        func(param)
+        if (props.onClick) {
+            console.log('Use "func={yourFunction}" instead of "onClick={yourFunction}",')
+            console.log('if you have any argument in the function, put in "param={yourArg}",')
+            return console.log('for more than one arg, put in one array "param={[funcArg, "foo", 123, false]}".');
+        }
+        if (func.name !== 'sendMe' && param === '/') {
+            console.log(`Maybe you forgot to set your function param? Pass it using "param={yourParam}" or "param='yourStringParam'" in your button component.`)
+        }
+        switch (Object.prototype.toString.call(param)) {
+            case '[object Boolean]':
+                return func(param);
+            case '[object String]':
+                return func(param);
+            case '[object Number]':
+                return func(param);
+            case '[object Function]':
+                return func(param);
+            case '[object Array]':
+                return func(...param);
+            default: return console.log("Hey the value of the 'param' prop is", [Object.prototype.toString.call(param).split(' ')[1].split(']')[0]], 'you need to pass a "Boolean", "String", "Number", "Function", "Undefined" (will be the default param ("/")) or "Array" for this to work.')
+        }
     }
 
     return (
