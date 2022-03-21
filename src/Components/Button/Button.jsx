ee1f22344367f07401d0abee2fe1747c3ded3870
Luigi Minardi <lugminsinlag@gmail.com>
2022-03-21 05:01:28 +0100

    feat: adding event option to button component

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -10,32 +10,39 @@ const Button = (props) => {
         navigate(url);
     }
 
-    const sendMeOrCustomFunction = (func = sendMe, param = '/') => {
+    const sendMeOrCustomFunction = (func = sendMe, paramArray) => {
+        let param = paramArray[1] ? paramArray[1] : '/';
+
         if (props.onClick) {
             console.log('Use "func={yourFunction}" instead of "onClick={yourFunction}",')
+            console.log('if you want to use a event set the param as "e", if passing one array set "e" as the first arg,')
             console.log('if you have any argument in the function, put in "param={yourArg}",')
             return console.log('for more than one arg, put in one array "param={[funcArg, "foo", 123, false]}".');
-        }
-        if (func.name !== 'sendMe' && param === '/') {
-            console.log(`Maybe you forgot to set your function param? Pass it using "param={yourParam}" or "param='yourStringParam'" in your button component.`)
         }
         switch (Object.prototype.toString.call(param)) {
             case '[object Boolean]':
                 return func(param);
             case '[object String]':
+                if (param === 'e') {
+                    return func(paramArray[0]);
+                }
                 return func(param);
             case '[object Number]':
                 return func(param);
             case '[object Function]':
                 return func(param);
             case '[object Array]':
+                if (param[0] === 'e') {
+                    param.splice(0, 1);
+                    return func(paramArray[0], ...param);
+                }
                 return func(...param);
             default: return console.log("Hey the value of the 'param' prop is", [Object.prototype.toString.call(param).split(' ')[1].split(']')[0]], 'you need to pass a "Boolean", "String", "Number", "Function", "Undefined" (will be the default param ("/")) or "Array" for this to work.')
         }
     }
 
     return (
-        <S.Button {...props} onClick={() => { sendMeOrCustomFunction(props.func, props.param) }}>{props.children}</S.Button>
+        <S.Button {...props} onClick={(e) => { sendMeOrCustomFunction(props.func, [e, props.param]) }}>{props.children}</S.Button>
     )
 }
 
